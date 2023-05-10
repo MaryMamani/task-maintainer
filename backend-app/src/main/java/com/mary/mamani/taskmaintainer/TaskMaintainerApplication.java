0d16861319340bd2e747056ec2623c916070fa8f
Mary Mamani <mary.mamani@fundacion-jala.org>
2023-05-10 17:08:47 -0500

    Initial backend structure

---
+++ backend-app/src/main/java/com/mary/mamani/taskmaintainer/TaskMaintainerApplication.java
@@ -0,0 +1,13 @@
+package com.mary.mamani.taskmaintainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMaintainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMaintainerApplication.class, args);
+	}
+
+}
---
+++ backend-app/src/test/java/com/mary/mamani/taskmaintainer/TaskMaintainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mary.mamani.taskmaintainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMaintainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
